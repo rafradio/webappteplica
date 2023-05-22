65cb6aa49a9f85f063483b487b0ca49a20aaf7bd
Rafail Abdyushev <rafaelab@mail.ru>
2023-05-22 23:04:57 +0300

    django app

--- teplica/zavod/main/views.py
+++ teplica/zavod/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Cities
+from .models import Goods
 from django.http import HttpResponse
 from django.views.generic import DetailView, UpdateView, DeleteView
 from .models import ClientCall
@@ -7,9 +8,16 @@ from .forms import ClientCallForm
 
 def index(request):
     cities = Cities.objects.all()
+    goods = Goods.objects.all()
+    goodsList = list(goods)
+    goodsListSite = []
+    for i in range(4):
+        goodsListSite.append(goodsList[i])
+    print(goodsListSite[3].name)
     data = {
         'cities': cities,
-        'firstCity': next(iter(cities))
+        'firstCity': next(iter(cities)),
+        'goods': goodsListSite
         }
     return render(request, 'main/index.html', data)
 
